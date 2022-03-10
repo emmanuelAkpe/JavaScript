1d8f58f120807acd3addc0e8b10bd43eef43356e
Emmanuel Akpe <emmanuelakpe1@gmail.com>
2022-03-10 15:08:14 +0000

    Day 3

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let firstName = "star"
+
+let lastName = 'Emak'
+
+let age = 25
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+console.log("my name is " + firstName +' '+ lastName +" i am " + age + ' old' );
+
+
+console.log(`my name is ${firstName} and i am ${age} years old`);
+
+
+// Arrays
+
+let fruits = ["mango", "pawpaw", "orange"]
+
+console.log(fruits.join(","));
